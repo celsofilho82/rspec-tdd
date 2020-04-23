55cae2c543c526fb9956b4f7bcc4c28f2708a288
Celso Ribeiro <celsofilho82@gmail.com>
2020-04-23 15:25:07 -0300

    Starting with fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :all
+
+  it "Create a Customer without fixtures" do
+    subject.name = "Celso Ribeiro"
+    subject.email = "celso@gmai.com"
+    subject.save
+    expect(subject.name).to eq("Celso Ribeiro")  
+  end
+
+  it "Create a Customer with fixtures" do
+    customer = customers(:jackson)
+    expect(customer.name).to eq("Jackson Pires")  
+  end
+  
 end
