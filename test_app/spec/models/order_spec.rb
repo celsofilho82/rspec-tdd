00e926e8e2b2651742d48e8f1b26a76592a6ed4b
Celso Ribeiro <celsofilho82@gmail.com>
2020-04-29 09:11:36 -0300

    Using FactoryBot create list

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe Order, type: :model do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)  
   end
+
+  # Usandos listas para criar mais de um item da fábrica
+  it 'Has three orders' do
+    # Usuando o create_list para gerar 3 pedidos na fábrica orders
+    orders = create_list(:order, 3)
+    expect(orders.count).to eq(3)  
+  end
   
 end
