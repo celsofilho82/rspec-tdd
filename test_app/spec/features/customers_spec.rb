bd7d7ff5b3a5494270dd5c842d746a328e6d2ccf
Celso Ribeiro <celsofilho82@gmail.com>
2020-05-07 22:34:14 -0300

    Install and configure selenium-webdriver, chromedriver-helper and take screenshot of the test

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.feature "Customers", type: :feature do
+# Ativando o suporte a javascript com o parâmetro js: true
+RSpec.feature "Customers", type: :feature, js: true do
   
   it "Navegando na pagina /customers" do
     visit(customers_path)
     # print page.html -> Imprime a página html no conseole
     # save_and_open_page -> Salva a página visitada em um determinado diretório ../tmp/capybara/capybara-***.html
-    page.save_screenshot('screenshot.png')
+    # page.save_screenshot('screenshot.png') -> Salva um screenshot da página no diretório ../tmp/capybara/[nome do screenshot]
     expect(page).to have_current_path(customers_path) 
   end
 
