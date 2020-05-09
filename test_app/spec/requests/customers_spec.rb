600893d33281cce7a3b77dd7e7f6d482d9452a26
Celso Ribeiro <celsofilho82@gmail.com>
2020-05-09 19:41:45 -0300

    RSpec JSON POST

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -25,6 +25,27 @@ RSpec.describe "Customers", type: :request do
         )
       end
     end
-    
+
+    context "create" do
+      it "POST /customers" do
+        member = create(:member)
+        login_as(member, scope: :member)
+
+        headers = { "ACCEPT" => "application/json"}
+
+        customers_params = attributes_for(:customer)
+
+        post "/customers.json", params: { customer: customers_params }, headers: headers
+
+        expect(response).to have_http_status(201)
+        
+        expect(response.body).to include_json(
+          id: /\d/,
+          name: customers_params[:name],
+          email: customers_params[:email]
+        )  
+      end
+    end
+  
   end
 end
